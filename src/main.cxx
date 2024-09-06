ce23efc9f55676dd2cefaed9240077a5ab6a81c0
Bharathkumar Radhakrishnan <r.bharathkumar@outlook.com>
2024-09-06 12:27:23 +0200

    Code now works perfectly to get Wigner semicircle
    Currently we are calculating the entire history of all walkers and writing it as a nested list in a Python file that can be used to do visualisations. First we need to make it more memory efficient by not maintaining the entire history in the RAM. Instead we only maintain two lists, the current step and the next step, and then after every time iteration we write to the file. That way we are not in need of exhorbitantly high amounts of RAM

--- src/main.cxx
+++ src/main.cxx
@@ -10,7 +10,7 @@
 #include"boost/multi_array.hpp"
 
 
-#define PREC 3000
+#define PREC 500
 
 
 mpf_class vanderMonde(boost::multi_array<mpf_class, 1> evals){
@@ -25,56 +25,83 @@ mpf_class vanderMonde(boost::multi_array<mpf_class, 1> evals){
     return vm;
 }
 
-mpfr_rnd_t rnd = mpfr_get_default_rounding_mode();
-
 mpf_class expV(float ccharge, float beta, boost::multi_array<mpf_class, 1> evals){
     mpfr_t ccharger, betar, betapr;
     mpfr_init(ccharger);
-    mpfr_set_flt(ccharger, ccharge, rnd);
+    mpfr_set_flt(ccharger, ccharge, MPFR_RNDN);
     mpfr_init(betar);
-    mpfr_set_flt(betar, beta, rnd);
+    mpfr_set_flt(betar, beta, MPFR_RNDN);
     mpfr_init(betapr);
-    mpfr_set_flt(betapr, 4*M_PI*M_PI/beta, rnd);
+    mpfr_set_flt(betapr, 4*M_PI*M_PI/beta, MPFR_RNDN);
     
     // This implements Z = sum(exp(lambda beta)) and Zt = sum(exp(lambda beta,))
     mpfr_t Z;
-    mpfr_init_set_str(Z, "0", 10, rnd); // Initialise Z to 0 in base 10
+    mpfr_init_set_str(Z, "0", 10, MPFR_RNDN); // Initialise Z to 0 in base 10
     mpfr_t temp;
     mpfr_init(temp);
 
     mpfr_t Zt;
-    mpfr_init_set_str(Zt, "0", 10, rnd); // Initialise Z to 0 in base 10
+    mpfr_init_set_str(Zt, "0", 10, MPFR_RNDN); // Initialise Z to 0 in base 10
     mpfr_t tempt;
     mpfr_init(tempt);
 
     for (auto x: evals){
-        mpfr_set_f(temp, x.get_mpf_t(), rnd);
-        mpfr_mul(temp, temp, betar, rnd);
-        mpfr_exp(temp, temp, rnd);
-        mpfr_add(Z, Z, temp, rnd);
+        mpfr_set_f(temp, x.get_mpf_t(), MPFR_RNDN);
+        mpfr_mul(temp, temp, betar, MPFR_RNDN);
+        mpfr_exp(temp, temp, MPFR_RNDN);
+        mpfr_add(Z, Z, temp, MPFR_RNDN);
 
-        mpfr_set_f(tempt, x.get_mpf_t(), rnd);
-        mpfr_mul(tempt, tempt, betapr, rnd);
-        mpfr_exp(tempt, tempt, rnd);
-        mpfr_add(Zt, Zt, tempt, rnd);
+        mpfr_set_f(tempt, x.get_mpf_t(), MPFR_RNDN);
+        mpfr_mul(tempt, tempt, betapr, MPFR_RNDN);
+        mpfr_exp(tempt, tempt, MPFR_RNDN);
+        mpfr_add(Zt, Zt, tempt, MPFR_RNDN);
     }
     mpfr_printf ("Z is %.60Rf\n", Z);
     mpfr_printf ("Zt is %.60Rf\n", Zt);
+    // mpfr_clears(temp,tempt);
     
 
     // This implements exp(Z - Zt)
-    mpfr_t potential;
-    mpfr_init(potential);
-    mpfr_set(potential, Z, rnd);
-    mpfr_sub(potential, potential, Zt, rnd);
-    mpfr_exp(potential, potential, rnd);
+    // mpfr_t potential;
+    // mpfr_init(potential);
+    // mpfr_set(potential, Z, MPFR_RNDN);
+    // mpfr_sub(potential, potential, Zt, MPFR_RNDN);
+    // mpfr_exp(potential, potential, MPFR_RNDN);
+    // mpfr_printf ("potential is %.60Rf\n", potential);
 
 
     mpf_t returnValue;
     mpf_init(returnValue);
-    mpfr_get_f(returnValue, potential, rnd);
+    mpfr_get_f(returnValue, Z, MPFR_RNDN); // Need to change to potential
     return mpf_class(returnValue);
 }
+
+mpf_class gaussian(boost::multi_array<mpf_class, 1> evals){
+    // This implements Z = exp(-sum(x**2)) and Zt = sum(exp(lambda beta,))
+    mpfr_t Z;
+    mpfr_init_set_str(Z, "0", 10, MPFR_RNDN); // Initialise Z to 0 in base 10
+    
+    mpfr_t temp;
+    mpfr_init(temp);
+    for (auto x: evals){
+        mpfr_set_f(temp, x.get_mpf_t(), MPFR_RNDN); // eval[i]
+        mpfr_mul(temp, temp, temp, MPFR_RNDN); // eval[i]**2
+        mpfr_sub(Z, Z, temp, MPFR_RNDN); // Z - eval[i]**2
+    }
+    mpfr_clear(temp);
+    mpfr_exp(Z, Z, MPFR_RNDN); // Z <- exp(Z)
+    
+    mpf_t returnValue;
+    mpf_init(returnValue);
+    mpfr_get_f(returnValue, Z, MPFR_RNDN); // Need to change to potential
+    mpfr_clear(Z);
+    return mpf_class(returnValue);
+}
+
+mpf_class wigner(boost::multi_array<mpf_class, 1> evals){
+    return vanderMonde(evals) * gaussian(evals);
+}
+
 
 unsigned long long int read_urandom()
 {
@@ -91,46 +118,98 @@ unsigned long long int read_urandom()
 }
 
 int main(){
-    mpf_set_default_prec(PREC);
     std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+    mpf_set_default_prec(PREC);
+    mpfr_set_default_prec(PREC);
+    std::cout<<"Precision is: "<<mpfr_get_default_prec()<<std::endl;
     gmp_randclass rand (gmp_randinit_default);
 
-    mpf_class low, high;
-    low = 0;
-    high = 10;
+    mpf_class low, high, step_size;
+    low = -5;
+    high = 5;
+    step_size = 0.1;
 
-    // Create a 3D array that is time x id x dim
-    int time = 3;
-    int id = 4;
-    int dim = 100;
+    // Create a 3D array that is maxTime x numWalkers x dim
+    int maxTime = 100000;
+    int numWalkers = 5;
+    int dim = 50;
     typedef boost::multi_array<mpf_class, 3> array_type; // 3 here is the depth.
-    boost::array<array_type::index, 3> shape = {{ time, id, dim }}; // 3 here seems to do nothing as long as it is greater than depth. 
+    boost::array<array_type::index, 3> shape = {{ maxTime, numWalkers, dim }}; // 3 here seems to do nothing as long as it is greater than depth. 
     array_type data(shape);
     
     // Randomly initialise the first time step
-    // for(int i = 0; i < id; i++){
-    //     for(int j = 0; j < dim; j++){
-    //         rand.seed(read_urandom());
-    //         data[0][i][j] = (high - low)*rand.get_f() + low;
-    //     }
-    // }
-    for(int i = 0; i < id; i++){
+    for(int i = 0; i < numWalkers; i++){
         for(int j = 0; j < dim; j++){
             rand.seed(read_urandom());
             data[0][i][j] = (high - low)*rand.get_f() + low;
         }
     }
-    std::cout<<data[0][0][0]<<std::endl;
-    std::cout<<expV(1, 6.3, data[0][0]);
+
+    // Evolve the walks
+    typedef boost::multi_array<mpf_class, 1> step_type; // 1 here is the depth.
+    boost::array<step_type::index, 1> stepShape = {{ dim }};
+    step_type nextStep(stepShape);
+    for(int time = 1; time < maxTime; time++){
+        for(int i = 0; i < numWalkers; i++){
+            for(int d = 0; d < dim; d++){
+                nextStep[d] = data[time-1][i][d] + (2*step_size)*rand.get_f() - step_size;
+            }
+            mpf_class ratioPotential = wigner(nextStep)/wigner(data[time-1][i]);
+            mpf_class decisionToss = rand.get_f();
+
+            if (i == 0 && time % 500 == 0){
+                std::cout<<"ratioPotential at time = "<<time<<" is = "<<ratioPotential<<std::endl;
+            }
+
+            if (decisionToss > ratioPotential){
+                    data[time][i] = data[time - 1][i];
+            }
+            else{
+                data[time][i] = nextStep;
+            }
+        }
+    }
+
+    if(dim == 1){
+        for(int i = 0; i < numWalkers; i++){
+            std::cout<<"ID "<<i;
+            mpf_class sum = 0;
+            for(int time = 0; time < maxTime; time++){
+                sum += data[time][i][0];
+            }
+            sum = sum / maxTime;
+            std::cout<<" mean = "<<sum;
+            mpf_class squareExpectation = 0;
+            for(int time = 0; time < maxTime; time++){
+                squareExpectation += (data[time][i][0] * data[time][i][0]) / maxTime;
+            }
+            squareExpectation -= (sum*sum);
+            std::cout<<", var = "<<squareExpectation<<std::endl;
+        }
+    }
+
+    std::ofstream outputFile;
+    outputFile.open("/home/users/r/radhakrb/cardy/data/out.py");
+    outputFile<<"data = [";
+    for(int time = 0; time < maxTime; time++){
+        outputFile<<"[";
+        for(int id = 0; id < numWalkers; id++){
+            outputFile<<"[";
+            for(int d = 0; d < dim; d++){
+                outputFile<<data[time][id][d]<<",";
+            }
+            outputFile.seekp(-1, std::ios_base::cur);
+            outputFile<<"],";
+        }
+        outputFile.seekp(-1, std::ios_base::cur);
+        outputFile<<"],";
+    }
+    outputFile.seekp(-1, std::ios_base::cur);
+    outputFile<<"]";
 
 
-    // for(int i = 0; i < dimensions; i++){
-    // rand.seed(read_urandom());
-    // vec[i] = (high - low)*rand.get_f() + low;
-    // }
-    // std::cout<<vec[5]<<"\n";
-    // std::cout<<vanderMonde(vec)<<"\n";
-    // std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-    // std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "[µs]" << std::endl;
+
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+    std::cout << "Time elapsed = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "[µs]" << std::endl;
     return 0;
 }
